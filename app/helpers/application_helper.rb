18b59566dd929a9ad22623a593cfec6a95123da7
Renato Hernandez <renatolhernandez@hotmail.com>
2025-06-03 21:56:03 +0000

    Ajustes layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,8 @@ module ApplicationHelper
   include Pagy::Frontend
 
   def show_sidebar?
-    params[:controller].include?('sessions') || params[:controller].include?('public')
+    params[:controller].include?("sessions") ||
+    params[:controller].include?("public") ||
+     params[:controller].include?("devise_invitable/registrations")
   end
 end
